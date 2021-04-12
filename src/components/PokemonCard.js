a0929432adb219c1bda95490f9b086a90e7d6a55
Doug <doug.kerzner@gmail.com>
2021-04-12 14:39:05 -0400

    First Deliv Hit

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,20 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import { Card } from "semantic-ui-react";
 
-function PokemonCard() {
+function PokemonCard(props) {
+  const [toggleCard, setToggleCard] = useState(false)
+
+  function handleToggle(){
+    setToggleCard(toggleCard => !toggleCard)
+  }
+
   return (
     <Card>
-      <div>
+      <div onClick={handleToggle}>
         <div className="image">
-          <img alt="oh no!" />
+           {!toggleCard ?<img src={props.sprites.front} alt="oh no!" /> :<img src={props.sprites.back} alt="oh no!" />} 
         </div>
         <div className="content">
-          <div className="header">POKEMON NAME HERE</div>
+          <div className="header">{props.name}</div>
         </div>
         <div className="extra content">
           <span>
             <i className="icon heartbeat red" />
-            POKEMON HP HERE hp
+            {props.hp}
           </span>
         </div>
       </div>
--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -2,10 +2,23 @@ import React from "react";
 import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
-function PokemonCollection() {
+function PokemonCollection({pokemonArray,setPokemonArray}) {
+ 
+
+  const pokemons = pokemonArray.map((pokemon) => 
+    (
+      <PokemonCard
+      key={pokemon.name}
+      name={pokemon.name}
+      hp={pokemon.hp}
+      sprites={pokemon.sprites}
+      />
+    )
+  )
+
   return (
     <Card.Group itemsPerRow={6}>
-      <h1>Hello From Pokemon Collection</h1>
+      {pokemons}
     </Card.Group>
   );
 }
